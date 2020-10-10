9e12d63a3f94ee324bb359226153416efa37a3dd
KaladaJumbo <wkalada1@gmail.com>
2020-10-10 18:53:03 -0400

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+function writeCards(array, string) {
+    
+    let output = []
+    for (let i = 0; i < array.length; i++) {
+        output.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+
+    return output
+
+}
+
+function countDown(num) {
+
+    let i = 0
+
+    while (i <= num) {
+        console.log(num)
+        num--
+    }
+    
+}
